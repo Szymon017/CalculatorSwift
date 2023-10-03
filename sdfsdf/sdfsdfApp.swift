2ecfd31a72adba4e40b3f9b1ec89580825a079f8
SzymonKacprzyk <szymon.kacprzyk5@gmail.com>
2023-10-03 12:59:39 +0200

    Initial Commit

---
+++ sdfsdf/sdfsdfApp.swift
@@ -0,0 +1,17 @@
+//
+//  sdfsdfApp.swift
+//  sdfsdf
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct sdfsdfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
