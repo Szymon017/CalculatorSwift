522ce39c597ac8e77cc1438e8ee50db6657b2057
SzymonKacprzyk <szymon.kacprzyk5@gmail.com>
2023-10-03 13:44:19 +0200

    Widok

---
+++ sdfsdf/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView.swift
+//  sdfsdf
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonView()
+}
---
+++ sdfsdf/Calculation.swift
@@ -0,0 +1,8 @@
+//
+//  Calculation.swift
+//  sdfsdf
+//
+//  Created by student on 03/10/2023.
+//
+
+import Foundation
